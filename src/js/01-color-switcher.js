6a1daf474da29d9017c934653fbaaa189ac9572f
Andrii <107649409+Andriy-Kond@users.noreply.github.com>
2023-01-24 23:55:59 +0200

    -test

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,55 +1,25 @@
-// const startBtn = document.querySelector('[data-start]');
-// const stopBtn = document.querySelector('[data-stop]');
-// const body = document.querySelector('body');
-
-// startBtn.addEventListener('click', colorSwitchStart);
-// stopBtn.addEventListener('click', colorSwitchStop);
-
-// let intervalId;
-// function colorSwitchStart() {
-//   startBtn.disabled = true;
-//   stopBtn.disabled = false;
-//   intervalId = setInterval(() => {
-//     body.style.backgroundColor = getRandomHexColor();
-//   }, 1000);
-// }
-
-// function colorSwitchStop() {
-//   startBtn.disabled = false;
-//   stopBtn.disabled = true;
-//   clearInterval(intervalId);
-// }
-
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-// }
-
 const startBtn = document.querySelector('[data-start]');
 const stopBtn = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
+
+startBtn.addEventListener('click', colorSwitchStart);
+stopBtn.addEventListener('click', colorSwitchStop);
+
 let intervalId;
-startBtn.addEventListener('click', onStartBtnClick);
-stopBtn.addEventListener('click', onStopBtnClick);
-
-function onStartBtnClick(evt) {
-  startBtn.setAttribute('disabled', '');
-  const isStopBtnActive = stopBtn.hasAttribute('disabled');
-
-  if (isStopBtnActive) {
-    stopBtn.removeAttribute('disabled', '');
-  }
-  intervalId = setInterval(bodyColorChange, 1000);
+function colorSwitchStart() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
 }
 
-function bodyColorChange() {
-  document.body.style.backgroundColor = getRandomHexColor();
+function colorSwitchStop() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  clearInterval(intervalId);
 }
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-function onStopBtnClick(evt) {
-  clearInterval(intervalId);
-  stopBtn.setAttribute('disabled', '');
-  startBtn.removeAttribute('disabled', '');
 }
